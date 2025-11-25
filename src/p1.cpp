54c47d7168d7d0d06be24fd1fe3a535c605b5f5b
toscaliao <tosca.liao@gmail.com>
2025-11-25 13:42:28 +0800

    something_

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,24 +1,3 @@
-/*
-## 題目 1：高塔建築師 (The Tower Architect) - 20分
-
-### 故事背景
-
-你的數學老師不僅想知道 5 個人排隊有幾種排法（階乘），還想知道如果是堆積木，第 1 層 1 個、第 2 層 2 個...第 5 層 5 個，總共需要幾個積木（連加）。請寫一個程式同時計算這兩個數值！
-
-### 題目說明
-
-讀取一個整數 `n`，計算並印出：
-
-1. **階乘 (Factorial)**：$n! = 1 times 2 times 3 dots times n$
-2. **連加 (Sum)**：$Sum(n) = 1 + 2 + dots + n$
-
-**要求：**
-
-1. **必須**實作並使用以下兩個遞迴函數：
-    - `long long factorial(int n)`：計算階乘。
-    - `long long sum(int n)`：計算連加。
-2. **限制：** 本題**嚴格禁止使用任何迴圈** (for, while, do-while)，違者不予計分。助教將會檢查關鍵字。
-*/
 #include <iostream>
 using namespace std;
 
--- src/p3.cpp
+++ src/p3.cpp
@@ -1,10 +1,21 @@
-// 題目 3：破解數學家的謎題 (Cracking the Mathematician's Puzzle) - 30分
-
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 int main() {
-    
-    
+    int n;
+    cin >> n;
+    for (int i = 2; i <= (n / 2); i++) {
+        if (isPrime(i) && isPrime(n - i)) {
+            cout << i << " " << n - i << endl;
+            break;
+        }
+    }
     return 0;
 }
