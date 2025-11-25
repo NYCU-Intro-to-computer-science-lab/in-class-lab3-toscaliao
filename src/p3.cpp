78e1ab4e26724654235ddde3137ad427f4284725
toscaliao <tosca.liao@gmail.com>
2025-11-25 13:54:45 +0800

    GUI 自動提交

--- src/p3.cpp
+++ src/p3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int n) {
+bool prime(int n) {
     if (n <= 1) return false;
     for (int i = 2; i * i <= n; ++i) {
         if (n % i == 0) return false;
@@ -12,7 +12,7 @@ int main() {
     int n;
     cin >> n;
     for (int i = 2; i <= (n / 2); i++) {
-        if (isPrime(i) && isPrime(n - i)) {
+        if (prime(i) && prime(n - i)) {
             cout << i << " " << n - i << endl;
             break;
         }
