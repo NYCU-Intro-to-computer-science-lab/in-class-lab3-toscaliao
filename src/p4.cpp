eb8e4fa72c3aeb82e47f9a88efbbf1a811860482
toscaliao <tosca.liao@gmail.com>
2025-11-25 13:50:13 +0800

    something_

--- src/p4.cpp
+++ src/p4.cpp
@@ -1,10 +1,34 @@
-// 題目 4：遞迴的足跡 (Footsteps of Recursion) - 30分
-
 #include <iostream>
 using namespace std;
 
+int fib(int n, int depth) {
+    for (int i = 0; i < depth; ++i) {
+        cout << "|--";
+    }
+    cout << "SEARCH fib(" << n << ")" << endl;
+
+    if (n == 1 || n == 2) {
+        for (int i = 0; i < depth; ++i) {
+            cout << "|--";
+        }
+        cout << "GET fib(" << n << ") = 1" << endl;
+        return 1;
+    }
+
+    int r1 = fib(n - 1, depth + 1);
+    int r2 = fib(n - 2, depth + 1);
+    int result = r1 + r2;
+
+    for (int i = 0; i < depth; i++) {
+        cout << "|--";
+    }
+    cout << "GET fib(" << n << ") = " << result << endl;
+
+    return result;
+}
 int main() {
-    
-    
+    int n = 0;
+    cin>>n;
+    cout<< fib(n, 0)<<endl;
     return 0;
 }
--- src/p5.cpp
+++ src/p5.cpp
@@ -3,9 +3,22 @@
 
 using namespace std;
 
+void hanoi(int n, char from, char to, char aux) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << from << " to " << aux << endl;
+        cout << "Move disk 1 from " << aux << " to " << to << endl;
+        return;
+    }
+    hanoi(n - 1, from, to, aux);
+    cout << "Move disk " << n << " from " << from << " to " << aux << endl;
+    hanoi(n - 1, to, from, aux);
+    cout << "Move disk " << n << " from " << aux << " to " << to << endl;
+    hanoi(n - 1, from, to, aux);
+}
 
 int main() {
-    
-
+    int n;
+    cin >> n;
+    hanoi(n, '1', '3', '2');
     return 0;
 }
